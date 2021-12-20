508c5fce6a4c5491233deae5b5ff5ec47a661ef0
Dudley D <94076825+Dudley-Domond@users.noreply.github.com>
2021-12-19 20:01:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/* Users should be able to see five buttons for the five boroughs (manhattan, brooklyn, queens, staten island, bronx) of New York City when they load the page
-Users should also be able to see an input box where they can put in a number of how many complaints they want to see
-When the user clicks on one of the five buttons, a list of complaints should be displayed on the page, according to the number they input AND the borough they clicked on
-If the user doesn't input any number, make the default be 10
-Remember, also, they only want complaints that were handled by the NYPD! (hint: consider filtering for a specific "agency" when making the API call)
-When the list of complaints is shown, each complaint should also have a button on it that reads something along the lines of "toggle police response"
-When the user clicks on on the "toggle police response" button, it should then toggle how the police responded to that particular complaint
-Make sure it only toggles the response for that one complaint, not the entire list! */
-
 const APP_TOKEN = "3nMPlHyMofEykN2kh1YXuq6vz";
 const NYPD_AGENCY = "New York City Police Department";
 const BOROUGHS = ["Brooklyn", "Manhattan", "Queens", "Bronx", "Staten Island"];
@@ -55,8 +46,6 @@ $(() => {
         "cursor" : "pointer"
     })
 
-    // add right margin and different BG color styling to every borough div
-
     backgroundColors.forEach((item, index) => {
         $(".boroughDiv").eq(index).css({
             "background-color" : item,
@@ -64,7 +53,6 @@ $(() => {
         })
     })
 
-// prevent default on every button and add input field after every h2 tag
 
     for(let i = 0; i < NUM_OF_BOROUGH_DIVS; i++) {
         let userInput;
@@ -85,7 +73,7 @@ $(() => {
                 "display" : "none"
             })
         })
-        // query each borough with specific information and display it in div when button is pressed
+        
         $(".getAnswers").eq(i).click(()=>{
             userInput = parseInt($("input:text").eq(i).val());
             paragraphStyle = 
@@ -130,7 +118,7 @@ $(() => {
                         })
                     }
                 }) 
-            } else /* if ($("input:text").eq(i).val("0") || isNaN(userInput) === true) */ {
+            } else 
                 $.ajax({
                     url: MY_URL,
                     type: "GET",
@@ -172,6 +160,4 @@ $(() => {
         "margin-left" : "1rem"
     })
     $("input:text").attr("placeholder", "Type your number here");
-
-    /* console.log(promise); */
 })
